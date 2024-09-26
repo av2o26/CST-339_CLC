7ef25e21d51501289c1312fd2b69bcd8f6893a55
Alex <av2o26@outlook.com>
2024-09-26 14:58:59 -0700

    Message

---
+++ src/main/java/com/gcu/Cst339ShopClcApplication.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst339ShopClcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst339ShopClcApplication.class, args);
+	}
+
+}
